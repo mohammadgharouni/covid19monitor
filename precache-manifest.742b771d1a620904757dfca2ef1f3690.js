97a4f292cc9fe3d28400b4991b4306eb045ba1ee
Mohamad Gharouni <m.gharouni@septaco.org>
2020-08-15 15:38:26 +0430

    Updates

---
+++ precache-manifest.742b771d1a620904757dfca2ef1f3690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ea79c54de0bf9fe03315347f08be73",
+    "url": "/covid19monitor/index.html"
+  },
+  {
+    "revision": "2647eaea06d99980ef82",
+    "url": "/covid19monitor/static/css/main.33325976.chunk.css"
+  },
+  {
+    "revision": "23ae2b5240e0b7c99ea0",
+    "url": "/covid19monitor/static/js/2.6a9545d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19monitor/static/js/2.6a9545d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2647eaea06d99980ef82",
+    "url": "/covid19monitor/static/js/main.6a84c64b.chunk.js"
+  },
+  {
+    "revision": "1439a2a3178d544f6bf9",
+    "url": "/covid19monitor/static/js/runtime-main.2e6c935e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19monitor/static/media/logo.5d5d9eef.svg"
+  }
+]);
